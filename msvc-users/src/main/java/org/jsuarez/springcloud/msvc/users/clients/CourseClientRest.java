432bc60eeaba66fdf7561a041c886fbae5b0950c
jdavi <jdavid94@hotmail.com>
2024-05-06 13:29:01 -0400

    Dockerfile and docker-compose.yml implementation

--- msvc-users/src/main/java/org/jsuarez/springcloud/msvc/users/clients/CourseClientRest.java
+++ msvc-users/src/main/java/org/jsuarez/springcloud/msvc/users/clients/CourseClientRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="msvc-courses", url = "localhost:8090")
+@FeignClient(name="msvc-courses", url = "${msvc.courses.url}")
 public interface CourseClientRest {
 
     @DeleteMapping("/delete-user/{id}")
